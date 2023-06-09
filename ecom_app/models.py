edd85477363f409b08ea299db73c4fe0f1183401
GautamSavsaviya <gautamsavasaviya222@gmail.com>
2023-06-09 22:36:54 +0530

    Create model named Product and also chage INSTALLED_APPS in settings.py file and also apply migrations using 'python manage.py migrate' command

--- ecom_app/models.py
+++ ecom_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    published_date = models.DateField()
