6bc5bd77c0a03398ac371c5e203afd28853991ed
GautamSavsaviya <gautamsavasaviya222@gmail.com>
2023-06-13 18:16:43 +0530

    Show products in slider by fatching it from the database

--- ecom_app/views.py
+++ ecom_app/views.py
@@ -1,10 +1,19 @@
+from math import ceil
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'ecom_app/index.html')
+    products = Product.objects.all()
+    n = len(products)
+    no_slide = n//4 + ceil(n/4 - (n//4))
+    params = {"product": products, "range": range(1,no_slide), "no_of_slide": no_slide}
+    print(products)
+
+    return render(request, 'ecom_app/index.html', params)
 
 
 def about(request):
