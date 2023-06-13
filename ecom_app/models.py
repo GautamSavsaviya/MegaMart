e2d8bffe07504d82bdb695c69f0609ba51828233
GautamSavsaviya <gautamsavasaviya222@gmail.com>
2023-06-13 11:45:00 +0530

    Do changes in Product model in model.py file

--- ecom_app/models.py
+++ ecom_app/models.py
@@ -6,5 +6,12 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, default="")
+    sub_category = models.CharField(max_length=100, default="")
+    price = models.IntegerField(default=0)
     description = models.CharField(max_length=200)
     published_date = models.DateField()
+    image = models.ImageField(upload_to='ecom_app/images', default="")
+
+    def __str__(self):
+        return self.product_name
